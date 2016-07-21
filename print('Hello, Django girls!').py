7266802951fb535c31c20994707ef0124ba1220f
m11ayden <morgan.elizabeth.hayden@gmail.com>
2016-07-21 14:08:24 -0400

    My Django Girls app, first commit

---
+++ print('Hello, Django girls!').py
@@ -0,0 +1,56 @@
+# print('Hello, Django girls!')
+# participant = {'name':'Morgan', 'country':'United States', 'fav language':'python', 'fav color':'green'}
+# lottory = [2, 7, 13, 10, 24]
+# if 3>2:
+# 	print("It works!")
+# if 5>2:
+# 	print('5 is indeed greater than 2')
+# else:
+# 	print('5 is not greaters than 2')
+# name="Morgan"
+# if name=="Brian":
+# 	print("Hey Brian")
+# elif name=="Morgan":
+# 	print("Hey Morgan")
+# else:
+# 	print("Hey anonymous")
+# volume = 90
+# if volume < 20:
+# 	print("It's kinda quiet")
+# elif 20 <= volume < 40:
+# 	print("It's nice for background music")
+# elif 40 <= volume < 60:
+# 	print("Perfect, I can hear all the details")
+# elif 60 <= volume < 80:
+# 	print("Nice for parties")
+# elif 80 <= volume < 100:
+# 	print("A bit loud!")
+# else:
+# 	print("My ears are hurting")
+#Change the Volume if it's too loud or too quiet
+# if volume < 20 or volume > 80:
+# 	volume=50
+# 	print("Thats better!")
+# def hi():
+# 	print ("Hi there!")
+# 	print("How are you?")
+# hi()
+# def hi(name):
+# 	if name == "Morgan":
+# 		print("Hi Morgan")
+# 	elif name == "Brian":
+# 		print("Hi Brian")
+# 	else:
+# 		print("Hi anonymous")
+# hi("Becca")
+def hi(name):
+	print ("Hi" + name + '!')
+girls = ['Morgan', 'Rachel', 'Becca', 'Molly']
+for name in girls:
+	hi(name)
+	print("Next girl")
+for i in range(1,6):
+	print(i)
+
+
+
